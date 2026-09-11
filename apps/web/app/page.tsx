9d4916bf8577c214c951ce658029b5b3810966b8
MURARI <tripathimurari599@gmail.com>
2026-09-11 23:23:31 +0530

    updates -2

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className='text-center'>
+        <h1 className='text-4xl font-bold'>ShopSphere</h1>
+        <p className='mt-4 text-grey-600'>your e-commerce marketplace</p>
+      </div>  
+    </main>
+  );
+}
